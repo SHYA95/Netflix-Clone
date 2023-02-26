a2ab6988abaeadf54250fa7d855c9095ee1cc830
SHYA95 <yassershrouk254@gmail.com>
2023-02-26 23:59:45 +0200

    Navbar commit

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+let sectionTitles:[String] = ["Trending movies","Popular","Trending TV","Upcoming movies","Top rated"]
+
 
 class HomeViewController: UIViewController {
     private let HomeFeedTables: UITableView = {
@@ -23,11 +25,25 @@ class HomeViewController: UIViewController {
         HomeFeedTables.delegate=self
         HomeFeedTables.dataSource=self
         
+        configureNavBar()
         
         let headerView = HeroHeaderUiView(frame: CGRect(x:  0, y: 0, width: view.bounds.width, height: 450))
         HomeFeedTables.tableHeaderView = headerView
     }
     
+    private func configureNavBar(){
+            
+            var image = UIImage(named: "netflix_logo")
+            image = image?.withRenderingMode(.alwaysOriginal)
+            navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done , target: self, action: nil)
+        
+            navigationItem.rightBarButtonItems = [ UIBarButtonItem(image:UIImage(systemName: "person") , style: .done, target: self, action: nil) , UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationController?.navigationBar.tintColor = .white
+        
+        }
+
+
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -38,8 +54,9 @@ class HomeViewController: UIViewController {
 }
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
+        return sectionTitles.count
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -47,18 +64,39 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
             return UITableViewCell()
-        
+            
         }
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-         return 40
+        
+        func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 40
+        }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int)  {
+        guard let header = view as?UITableViewHeaderFooterView else {return}
+        header.textLabel?.font = .systemFont(ofSize: 20, weight: .semibold)
+        
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x+20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.tintColor = .white
+        
     }
+    
+    
+    
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return sectionTitles[section]
+        }
+        
+    //for hidden the navbar when scrolling the screen
+    //custom algorisem
+        func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            let defaultOffset = view.safeAreaInsets.top
+            let offset = scrollView.contentOffset.y + defaultOffset
+            navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0,-offset))
+        }
     }
-    
-    
-    
 
